05020f68d74fa3231f126bf0e8a3739cd9777acf
sumitbansode360 <sumitbansode360@gmail.com>
2025-05-03 20:53:47 +0530

    final changes

--- docprj/settings.py
+++ docprj/settings.py
@@ -36,7 +36,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "editly-ksgl.onrender.com"]
+
+CSRF_TRUSTED_ORIGINS = ["https://editly-ksgl.onrender.com"]
 
 
 # Application definition
